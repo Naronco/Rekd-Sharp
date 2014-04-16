f0f4c296f84cce17ad9cf4acf72491860b5e838c
WebFreak001 <janju007@web.de>
2014-04-16 17:41:26 +0200

    Added Bezier Curve

---
+++ RekdEngine/UtilMath/Algorithm/BezierCurve.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using M = System.Math;
+
+namespace RekdEngine.UtilMath.Algorithm
+{
+	public class BezierCurve
+	{
+		public List<Vector2f> Points;
+		protected Dictionary<float, Vector2f> Saved;
+
+		public BezierCurve()
+		{
+			Points = new List<Vector2f>();
+			Saved = new Dictionary<float, Vector2f>();
+		}
+
+		public Vector2f Calculate(float t, bool useMem, bool forceRecalc)
+		{
+			if (useMem && !forceRecalc)
+			{
+				if (Saved.ContainsKey(t)) return Saved[t];
+			}
+
+			float b = 0;
+			Vector2f res = new Vector2f();
+			int n = Points.Count - 1;
+			for (int i = 0; i <= n; i++)
+			{
+				b = MathExt.CalcBinomialCoefficient(n, i);
+				res += b * (float)M.Pow(1 - t, (float)(n - i)) * (float)M.Pow(t, (float)i) * Points[i];
+			}
+			if (useMem) Saved[t] = res;
+			return res;
+		}
+
+		public Vector2f Calculate(float t)
+		{
+			return Calculate(t, false, false);
+		}
+	}
+}
