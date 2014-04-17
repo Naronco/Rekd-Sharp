b7ded9f6115a12761b889530ca9a96003869ce98
WebFreak001 <janju007@web.de>
2014-04-17 15:37:19 +0200

    added Vector classes

---
+++ RekdEngine/UtilMath/Vector3f.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using M = System.Math;
+
+namespace RekdEngine.UtilMath
+{
+	public struct Vector3f
+	{
+		public float X, Y, Z;
+
+		public Vector3f(float X = 0, float Y = 0, float Z = 0)
+		{
+			this.X = X;
+			this.Y = Y;
+			this.Z = Z;
+		}
+
+		public Vector3f(Vector3f other)
+		{
+			X = other.X;
+			Y = other.Y;
+			Z = other.Z;
+		}
+
+		public Vector3f Clear()
+		{
+			X = Y = Z = 0;
+			return this;
+		}
+
+		public Vector3f Set(float X, float Y, float Z)
+		{
+			this.X = X;
+			this.Y = Y;
+			this.Z = Z;
+			return this;
+		}
+
+		public Vector3f Set(Vector3f other)
+		{
+			X = other.X;
+			Y = other.Y;
+			Z = other.Z;
+			return this;
+		}
+
+		public static bool operator ==(Vector3f A, Vector3f B)
+		{
+			return A.X == B.X && A.Y == B.Y && A.Z == B.Z;
+		}
+
+		public static bool operator !=(Vector3f A, Vector3f B)
+		{
+			return A.X != B.X || A.Y != B.Y || A.Z != B.Z;
+		}
+
+		public static Vector3f operator -(Vector3f v)
+		{
+			return new Vector3f(-v.X, -v.Y, -v.Z);
+		}
+
+		public static Vector3f operator +(Vector3f A, Vector3f B)
+		{
+			return new Vector3f(A.X + B.X, A.Y + B.Y, A.Z + B.Z);
+		}
+
+		public static Vector3f operator -(Vector3f A, Vector3f B)
+		{
+			return new Vector3f(A.X - B.X, A.Y - B.Y, A.Z - B.Z);
+		}
+
+		public static Vector3f operator *(Vector3f v, float scalar)
+		{
+			return new Vector3f(v.X * scalar, v.Y * scalar, v.Z * scalar);
+		}
+
+		public static Vector3f operator /(Vector3f v, float scalar)
+		{
+			return new Vector3f(v.X / scalar, v.Y / scalar, v.Z / scalar);
+		}
+
+		public static Vector3f operator *(float scalar, Vector3f v)
+		{
+			return new Vector3f(v.X * scalar, v.Y * scalar, v.Z * scalar);
+		}
+
+		public float GetSquaredLength()
+		{
+			return X * X + Y * Y + Z * Z;
+		}
+
+		public float GetLength()
+		{
+			return (float)M.Sqrt(X * X + Y * Y + Z * Z);
+		}
+
+		public static float GetSquaredDistance(Vector3f A, Vector3f B)
+		{
+			float xd = A.X - B.X;
+			float yd = A.Y - B.Y;
+			float zd = A.Z - B.Z;
+			return xd * xd + yd * yd + zd * zd;
+		}
+
+		public float GetSquaredDistance(Vector3f other)
+		{
+			return GetSquaredDistance(this, other);
+		}
+
+		public static float GetDistance(Vector3f A, Vector3f B)
+		{
+			float xd = A.X - B.X;
+			float yd = A.Y - B.Y;
+			float zd = A.Z - B.Z;
+			return (float)M.Sqrt(xd * xd + yd * yd + zd * zd);
+		}
+
+		public float GetDistance(Vector3f other)
+		{
+			return GetDistance(this, other);
+		}
+
+		public Vector3f Normalize()
+		{
+			return this * (1 / GetLength());
+		}
+
+		public float Dot(Vector3f v)
+		{
+			return X * v.X + Y * v.Y + Z * v.Z;
+		}
+
+		public Vector3f Cross(Vector3f v)
+		{
+			return new Vector3f(Y * v.Z - Z * v.Y, Z * v.X - X * v.Z, X * v.Y - Y * v.X);
+		}
+	}
+}
---
+++ RekdEngine/UtilMath/Vector4f.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using M = System.Math;
+
+namespace RekdEngine.UtilMath
+{
+	public struct Vector4f
+	{
+		public float X, Y, Z, W;
+
+		public Vector4f(float X = 0, float Y = 0, float Z = 0, float W = 0)
+		{
+			this.X = X;
+			this.Y = Y;
+			this.Z = Z;
+			this.W = W;
+		}
+
+		public Vector4f(Vector4f other)
+		{
+			X = other.X;
+			Y = other.Y;
+			Z = other.Z;
+			W = other.W;
+		}
+
+		public Vector4f Clear()
+		{
+			X = Y = Z = W = 0;
+			return this;
+		}
+
+		public Vector4f Set(float X, float Y, float Z)
+		{
+			this.X = X;
+			this.Y = Y;
+			this.Z = Z;
+			this.W = W;
+			return this;
+		}
+
+		public Vector4f Set(Vector4f other)
+		{
+			X = other.X;
+			Y = other.Y;
+			Z = other.Z;
+			W = other.W;
+			return this;
+		}
+
+		public static bool operator ==(Vector4f A, Vector4f B)
+		{
+			return A.X == B.X && A.Y == B.Y && A.Z == B.Z && A.W == B.W;
+		}
+
+		public static bool operator !=(Vector4f A, Vector4f B)
+		{
+			return A.X != B.X || A.Y != B.Y || A.Z != B.Z || A.W != B.W;
+		}
+
+		public static Vector4f operator -(Vector4f v)
+		{
+			return new Vector4f(-v.X, -v.Y, -v.Z, v.W);
+		}
+
+		public static Vector4f operator +(Vector4f A, Vector4f B)
+		{
+			return new Vector4f(A.X + B.X, A.Y + B.Y, A.Z + B.Z, A.W);
+		}
+
+		public static Vector4f operator -(Vector4f A, Vector4f B)
+		{
+			return new Vector4f(A.X - B.X, A.Y - B.Y, A.Z - B.Z, A.W);
+		}
+
+		public static Vector4f operator *(Vector4f v, float scalar)
+		{
+			return new Vector4f(v.X * scalar, v.Y * scalar, v.Z * scalar, v.W);
+		}
+
+		public static Vector4f operator /(Vector4f v, float scalar)
+		{
+			return new Vector4f(v.X / scalar, v.Y / scalar, v.Z / scalar, v.W);
+		}
+
+		public static Vector4f operator *(float scalar, Vector4f v)
+		{
+			return new Vector4f(v.X * scalar, v.Y * scalar, v.Z * scalar, v.W);
+		}
+
+		public float GetSquaredLength()
+		{
+			return X * X + Y * Y + Z * Z + W * W;
+		}
+
+		public float GetLength()
+		{
+			return (float)M.Sqrt(X * X + Y * Y + Z * Z + W * W);
+		}
+
+		public static float GetSquaredDistance(Vector4f A, Vector4f B)
+		{
+			float xd = A.X - B.X;
+			float yd = A.Y - B.Y;
+			float zd = A.Z - B.Z;
+			return xd * xd + yd * yd + zd * zd;
+		}
+
+		public float GetSquaredDistance(Vector4f other)
+		{
+			return GetSquaredDistance(this, other);
+		}
+
+		public static float GetDistance(Vector4f A, Vector4f B)
+		{
+			float xd = A.X - B.X;
+			float yd = A.Y - B.Y;
+			float zd = A.Z - B.Z;
+			float wd = A.W - B.W;
+			return (float)M.Sqrt(xd * xd + yd * yd + zd * zd + wd * wd);
+		}
+
+		public float GetDistance(Vector4f other)
+		{
+			return GetDistance(this, other);
+		}
+
+		public Vector4f Normalize()
+		{
+			return this * (1 / GetLength());
+		}
+
+		public float Dot(Vector4f v)
+		{
+			return X * v.X + Y * v.Y + Z * v.Z + W * v.W;
+		}
+
+		public Vector4f Cross(Vector4f v)
+		{
+			return new Vector4f(Y * v.Z - Z * v.Y, Z * v.X - X * v.Z, X * v.Y - Y * v.X, W);
+		}
+	}
+}
